b686d1974a9e0b98e1c0b9acd1e93815fffd745f
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-11-28 15:02:25 -0300

    set username env variable to be required

--- web_server/app.py
+++ web_server/app.py
@@ -16,7 +16,7 @@ def main():
     config.namelist_remote_path.from_env("NAMELIST_REMOTE_PATH")
 
     config.hostname.from_env("SSH_HOST", required=True)
-    config.username.from_env("SSH_USER")
+    config.username.from_env("SSH_USER", required=True)
     config.password.from_env("SSH_PASS")
 
     wrf = container.service()
