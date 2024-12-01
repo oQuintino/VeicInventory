7d746ba6884435e40c1c54f40e0fbb627edd1fcb
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-01 08:37:01 -0300

    created a routes layer

--- web_server/routes/main_route.py
+++ web_server/routes/main_route.py
@@ -1,19 +1,62 @@
-from flask import Flask, render_template, request
-import f90nml
 import os
+
+import f90nml
 import paramiko
 from dotenv import load_dotenv
+from flask import Flask, render_template, request
 
-nml_params = ['frac_veic1','use_veic1','co_e_veic1','co2_e_veic1','ch4_e_veic1','frac_veic2','use_veic2','co_e_veic2','co2_e_veic2','ch4_e_veic2','frac_veic3','use_veic3','co_e_veic3','co2_e_veic3','ch4_e_veic3','frac_veic4a','use_veic4a','co_e_veic4a','co2_e_veic4a','ch4_e_veic4a','frac_veic4b','use_veic4b','co_e_veic4b','co2_e_veic4b','ch4_e_veic4b','frac_veic4c','use_veic4c','co_e_veic4c','co2_e_veic4c','ch4_e_veic4c','frac_veic5','use_veic5','co_e_veic5','co2_e_veic5','ch4_e_veic5','frac_veic6','use_veic6','co_e_veic6','co2_e_veic6','ch4_e_veic6']
+NML_PARAMS = (
+    "frac_veic1",
+    "use_veic1",
+    "co_e_veic1",
+    "co2_e_veic1",
+    "ch4_e_veic1",
+    "frac_veic2",
+    "use_veic2",
+    "co_e_veic2",
+    "co2_e_veic2",
+    "ch4_e_veic2",
+    "frac_veic3",
+    "use_veic3",
+    "co_e_veic3",
+    "co2_e_veic3",
+    "ch4_e_veic3",
+    "frac_veic4a",
+    "use_veic4a",
+    "co_e_veic4a",
+    "co2_e_veic4a",
+    "ch4_e_veic4a",
+    "frac_veic4b",
+    "use_veic4b",
+    "co_e_veic4b",
+    "co2_e_veic4b",
+    "ch4_e_veic4b",
+    "frac_veic4c",
+    "use_veic4c",
+    "co_e_veic4c",
+    "co2_e_veic4c",
+    "ch4_e_veic4c",
+    "frac_veic5",
+    "use_veic5",
+    "co_e_veic5",
+    "co2_e_veic5",
+    "ch4_e_veic5",
+    "frac_veic6",
+    "use_veic6",
+    "co_e_veic6",
+    "co2_e_veic6",
+    "ch4_e_veic6",
+)
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', params=nml_params)
-    
-    if request.method == 'POST':
+    if request.method == "GET":
+        return render_template("index.html", params=NML_PARAMS)
+
+    if request.method == "POST":
         # field1 = request.args.get("field1")
         # field2 = request.args.get("field2")
 
@@ -30,21 +73,20 @@ def index():
             else:
                 data[key] = 0
 
+        namelist_group = {"emission_vehicles": data}
 
-        namelist_group = {'emission_vehicles': data}
-
-        with open('namelist.emis', 'w') as nml_file:
+        with open("namelist.emis", "w") as nml_file:
             f90nml.write(namelist_group, nml_file)
 
-    return '''
+    return """
     <script>
     alert("dados recebidos com sucesso")
     window.location.replace("/")
-    </script>'''
+    </script>"""
 
-@app.route("/sendfile", methods=['GET'])
+
+@app.route("/sendfile", methods=["GET"])
 def send_file():
-
     load_dotenv()
 
     hostname = os.getenv("SSH_HOST")
@@ -67,5 +109,6 @@ def send_file():
     print(output)
 
     return output
+
 
 app.run(debug=True)
