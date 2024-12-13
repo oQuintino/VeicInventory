1b5aa4acb2fa05c5da4f122252ba53fafbb8c324
Matheus Quintino <matheus.qs2004@aluno.ifsc.edu.br>
2024-12-13 12:50:48 -0300

    refactored namelist title to be a LiteralString

--- web_server/services/namelist_creator.py
+++ web_server/services/namelist_creator.py
@@ -1,10 +1,11 @@
 from io import StringIO
 from types import MappingProxyType
+from typing import LiteralString
 
 
 
 class NamelistContentCreator:
-    def __init__(self, namelist_title: str):
+    def __init__(self, namelist_title: LiteralString):
         self.__title = namelist_title
 
     def create_namelist(self, namelist_data: MappingProxyType):
